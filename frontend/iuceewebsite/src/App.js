df5beb63f36ff870ae419b3b91518643252fb548
harshpatel <harshpatel97762@gmail.com>
2025-05-04 21:37:18 +0530

    iucee website wireframe

---
+++ frontend/iuceewebsite/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     {/* all page code will here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/iuceewebsite/src/component/mission&vissionpage/Missionpage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Missionpage = () => {
+  return (
+    <div>Missionpage</div>
+  )
+}
